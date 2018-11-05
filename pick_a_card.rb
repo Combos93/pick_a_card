fc71c8446a3f2b9fb11f36e6bc42a36238fd29d1
Combos <combos93@mail.ru>
2018-11-06 00:59:55 +0600

    try to fix the error

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -3,4 +3,11 @@ values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', 'Jok
 
 suites = ['Diamonds', 'Hearts' , 'Clubs', 'Spades']
 
+case suites
+when suites[0]
+puts "XXX"
+  # d = "\U2660"
+  # puts d.encode('utf-8')
+end
+
 puts "#{values.sample + " " + suites.sample}"

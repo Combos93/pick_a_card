a84170f49b836eff8d87fb253a50140b00357a2f
Combos93 <combos93@mail.ru>
2018-12-18 23:46:16 +0600

    Icons of suites done

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,7 @@
 # add a comment
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', 'Joker']
 
-suites = ['Diamonds', 'Hearts' , 'Clubs', 'Spades']
+suites = ['♦', '♥' , '♣', '♠']
 
-puts "#{values.sample + " " + suites.sample}"
+
+puts "#{values.sample + suites.sample}"

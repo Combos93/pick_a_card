1b829cf261e4af1aeefe75eb72b6e73950591881
Cobmos93 <combos93@mail.ru>
2019-01-21 21:57:56 +0600

    Title & copyright

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,6 @@
-# add a comment
+
+puts 'Программа Pick a Card. (c) goodprogrammer.ru Mikhail Berkovich'
+
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', 'Joker']
 
 suites = ['♦', '♥' , '♣', '♠']
